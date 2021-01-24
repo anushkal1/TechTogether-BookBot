5b5ee2f2b3f5668d275829665f3eb394223c1d31
anushka <46737299+anushkal1@users.noreply.github.com>
2021-01-23 17:26:30 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.login('ODAyNjU1ODM0NTM0MTgyOTIy.YAyZfw.KeDIRBS77srs3mqV7z8Sr91W7xg');
