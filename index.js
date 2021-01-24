fdf639ec9ce8f0c182c0de0e65171decc807aa68
anushka <46737299+anushkal1@users.noreply.github.com>
2021-01-24 14:49:33 -0800

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
-client.once('ready', () => {
-	console.log('Ready!');
-});
-
-client.login('ODAyNjU1ODM0NTM0MTgyOTIy.YAyZfw.KeDIRBS77srs3mqV7z8Sr91W7xg');
